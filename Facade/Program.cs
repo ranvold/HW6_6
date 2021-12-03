63551a10ecc9b78bccedb4b65d41161a8f76bc9f
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2021-12-03 14:22:11 +0200

    Add project files.

---
+++ Facade/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Facade.Examples
+{
+    // Mainapp test application 
+    class MainApp
+    {
+        public static void Main()
+        {
+            var water = new WaterManagingSubSystem();
+            var thermo = new Thermo();
+            var engine = new Engine();
+            var dryer = new Dryer();
+
+            var washingMachine = new WashingMachineFacade(water, thermo, engine, dryer);
+
+            // Linen
+            washingMachine.WashLinen();
+            Console.WriteLine();
+
+            // Silk
+            washingMachine.WashSilk();
+            Console.WriteLine();
+
+
+            // Wait for user 
+            Console.Read();
+        }
+    }
+
+
+    // "Subsystem ClassA" 
+    class WaterManagingSubSystem
+    {
+        public void FillWater(int litres)
+        {
+            Console.WriteLine("Fill with {0} litres of water", litres);
+        }
+        public void EmptyWater()
+        {
+            Console.WriteLine("Empty water tank");
+        }
+    }
+
+    // Subsystem ClassB" 
+    class Thermo
+    {
+        public void WarmUp(int degrees)
+        {
+            Console.WriteLine("Warm for {0} degrees", degrees);
+        }
+    }
+
+
+    // Subsystem ClassC" 
+    class Dryer
+    {
+        public void Dry(int seconds, int intensity)
+        {
+            Console.WriteLine("Drying {0} seconds with intensity {1}", seconds, intensity);
+        }
+    }
+    // Subsystem ClassD" 
+    class Engine
+    {
+        public void Rotate()
+        {
+            Console.WriteLine("Start rotating");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop rotating");
+        }
+    }
+    // "Facade" 
+    class WashingMachineFacade
+    {
+        private WaterManagingSubSystem _water;
+        private Thermo _thermo;
+        private Engine _engine;
+        private Dryer _dryer;
+
+        public WashingMachineFacade(WaterManagingSubSystem water, Thermo thermo, Engine engine, Dryer dryer)
+        {
+            _water = water;
+            _thermo = thermo;
+            _engine = engine;
+            _dryer = dryer;
+        }
+
+        public void WashLinen()
+        {
+            _water.FillWater(8);
+            _thermo.WarmUp(50);
+            _engine.Rotate();
+            _engine.Rotate();
+            _engine.Stop();
+            _water.EmptyWater();
+            _water.FillWater(10);
+            _thermo.WarmUp(30);
+            _engine.Rotate();
+            _engine.Rotate();
+            _engine.Rotate();
+            _engine.Stop();
+            _water.EmptyWater();
+            _dryer.Dry(40, 600);
+        }
+
+        public void WashSilk()
+        { 
+            _water.FillWater(9);
+            _thermo.WarmUp(55);
+            _engine.Rotate();
+            _engine.Stop();
+            _water.EmptyWater();
+            _water.FillWater(12);
+            _thermo.WarmUp(35);
+            _engine.Rotate();
+            _engine.Rotate();
+            _engine.Stop();
+            _water.EmptyWater();
+            _dryer.Dry(35, 500);
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace FactoryMethodExample
+{
+    //абстрактний клас творця, який має абстрактний метод FactoryMethod, що приймає тип продукта
+    public abstract class BMWFactory
+    {
+        public Car GetCar(string type)
+        {
+            Car car = CreateCar(type);
+
+            car.Configure();
+            car.AssembleBody();
+            car.InstallEngine();
+            car.Paint();
+            car.InstallWheels();
+
+            return car;
+        }
+        public abstract Car CreateCar(string type);
+    }
+
+    public class DeutchBMWFactory : BMWFactory
+    {
+        public override Car CreateCar(string type)
+        {
+            Car car;
+
+            if (type == "M3")
+                car = new DeutchM3();
+            else if (type == "535d")
+                car = new Deutch535d();
+            else if (type == "X6")
+                car = new DeutchX6();
+            else
+                car = new Deutch320i();
+
+            return car;
+        }
+    }
+
+    //абстрактний клас продукт
+    public abstract class Car
+    {
+        protected string Name = "BMW ";
+        protected string Engine = "Diesel";
+        protected string PaintColor = "White";
+        protected string Wheels = "16 inch";
+        protected string Body = "Sedan";
+
+        public void Configure()
+        {
+            Console.WriteLine("Configuring {0}", Name);
+            Console.WriteLine("Engine is: {0}", Engine);
+            Console.WriteLine("Body is: {0}", Body);
+            Console.WriteLine("PaintColor is: {0}", PaintColor);
+            Console.WriteLine("Wheels are: {0}", Wheels);
+        }
+
+        public void AssembleBody()
+        {
+            Console.WriteLine("Body is assembled");
+        }
+
+        public void InstallEngine()
+        {
+            Console.WriteLine("Engine is in its place");
+        }
+
+        public void Paint()
+        {
+            Console.WriteLine("Car is painted");
+        }
+
+        public void InstallWheels()
+        {
+            Console.WriteLine("Wheels are installed");
+        }
+    }
+
+    //конкретні продукти з різною реалізацією
+    public class DeutchM3 : Car
+    {
+        public DeutchM3()
+        {
+            Name += "M3";
+            Body = "Coupe";
+            Engine = "Gasoline";
+            PaintColor = "Silver";
+            Wheels = "19 inch";
+        }
+    }
+
+    public class Deutch535d : Car
+    {
+        public Deutch535d()
+        {
+            Name += "535d";
+            Wheels = "17 inch";
+        }
+    }
+
+    public class DeutchX6 : Car
+    {
+        public DeutchX6()
+        {
+            Name += "X6";
+            Body = "SUV";
+            Wheels = "17 inch";
+            PaintColor = "Black";
+        }
+    }
+
+    public class Deutch320i : Car
+    {
+        public Deutch320i()
+        {
+            Name += "320i";
+            Engine = "Gasoline";
+        }
+    }
+    class MainApp
+    {
+        static void Main()
+        {       //створюємо творця
+
+            BMWFactory factory = new DeutchBMWFactory();
+            factory.GetCar("M3");
+            Console.WriteLine();
+            factory.GetCar("320i");
+            Console.WriteLine();
+            factory.GetCar("X6");
+            Console.WriteLine();
+            factory.GetCar("535d");
+
+            Console.ReadKey();
+        }
+    }
+}
